9807e26de654bffc361c3d37f826478ca3119063
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2026-04-07 17:10:15 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+       var l = 0 ;
+       var max = 0;
+       for(var r = 1; r < prices.Length; r++)
+       {
+            if(prices[l] < prices[r])
+            {
+                max = Math.Max(max, prices[r] - prices[l]);       
+            }else{
+                l = r;    
+            }
+       }
+       return max;
+    }
+}
